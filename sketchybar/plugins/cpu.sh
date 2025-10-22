cc90dcfffce951f17fc22193f3506aa05846db63
Joakim Lorentz <joakim@murmel.se>
2025-10-22 14:02:38 +0200

    chore: removes sketchybar

--- sketchybar/plugins/cpu.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-TOPPROC=$(top -l  2 | grep -E "^CPU" | tail -1 | awk '{ print $3 + $5"%" }' | cut -d "." -f1)
-
-sketchybar --set $NAME label="$TOPPROC%"
--- sketchybar/plugins/front_app.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-if [ "$SENDER" = "front_app_switched" ]; then
-  sketchybar --set "$NAME" label="$INFO"
-fi
